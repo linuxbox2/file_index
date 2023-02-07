344480bb9c0deb0027f9e2d8210b8abe67bd8683
Matt Benjamin <mbenjamin@redhat.com>
2023-02-07 17:32:51 -0500

    more realistic test setup
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- main.cpp
+++ main.cpp
@@ -2,9 +2,17 @@
 // vim: ts=8 sw=2 smarttab ft=cpp
 
 #include "bucket_cache.h"
+#include <iostream>
+#include <fstream>
 #include <filesystem>
 #include <string>
+#include <random>
 #include <stdint.h>
+
+#undef FMT_HEADER_ONLY
+#define FMT_HEADER_ONLY 1
+#include <fmt/format.h>
+
 #include <gtest/gtest.h>
 
 namespace {
@@ -15,17 +23,61 @@ namespace {
   uint8_t lmdb_count = 3;
   std::string bucket1_name = "stanley";
   std::string bucket1_marker = ""; // start at the beginning
+
+  std::random_device rd;
+  std::mt19937 mt(rd());
+
+  std::string tdir1{"tdir1"};
+  std::uniform_int_distribution<> dist_1m(1, 1000000);
+  BucketCache* bc{nullptr};
 } // anonymous ns
+
+namespace sf = std::filesystem;
 
 TEST(BucketCache, SetupTDir1)
 {
+  sf::path tp{sf::path{bucket_root} / tdir1};
+  sf::remove_all(tp);
+  sf::create_directory(tp);
+
+  /* generate 1M unique files in random order */
+  std::string fbase{"file_"};
+  for (int ix = 0; ix < 100000; ++ix) {
+  retry:
+    auto n = dist_1m(mt);
+    sf::path ttp{tp / fmt::format("{}{}", fbase, n)};
+    if (sf::exists(ttp)) {
+      goto retry;
+    } else {      
+      std::ofstream ofs(ttp);
+      ofs << "data for " << ttp << std::endl;
+      ofs.close();
+    }
+  } /* for 1M */
+  
+  
+}
+
+TEST(BucketCache, InitBucketCache)
+{
+  bc = new BucketCache{bucket_root, database_root, max_buckets, max_lanes, lmdb_count};
 }
 
 TEST(BucketCache, ListTDir1)
 {
-  BucketCache bc{bucket_root, database_root, max_buckets, max_lanes, lmdb_count};
-  bc.list_bucket(bucket1_name, bucket1_marker); // XXX need a lambda to handle the output
+  bc->list_bucket(tdir1, bucket1_marker); // XXX need a lambda to handle the output
 }
+
+TEST(BucketCache, ListTDir2)
+{
+  bc->list_bucket(tdir1, bucket1_marker); // XXX need a lambda to handle the output
+}
+
+TEST(BucketCache, ListTDir3)
+{
+  bc->list_bucket(tdir1, bucket1_marker); // XXX need a lambda to handle the output
+}
+
 
 int main (int argc, char *argv[])
 {
