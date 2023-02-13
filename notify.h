4e154758a58b95c9c67dcb790600081a4e2e2d5d
Matt Benjamin <mbenjamin@redhat.com>
2023-02-13 16:55:18 -0500

    block in poll loop
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- notify.h
+++ notify.h
@@ -11,9 +11,14 @@
 #include <condition_variable>
 #include <chrono>
 #include <filesystem>
+#include <limits>
+#include <cstdlib>
 #include "unordered_dense.h"
+#include <unistd.h>
+#include <poll.h>
 #ifdef linux
 #include <sys/inotify.h>
+#include <sys/eventfd.h>
 #endif
 #undef FMT_HEADER_ONLY
 #define FMT_HEADER_ONLY 1
@@ -45,31 +50,98 @@ namespace file::listing {
 #ifdef linux
   class Inotify : public Notify
   {
-
     using wd_map_t = ankerl::unordered_dense::map<std::string, int>;
+    
+    static constexpr uint32_t rd_size = 8192;
+    static constexpr uint64_t sig_shutdown = std::numeric_limits<uint64_t>::max() - 0xdeadbeef;
     static constexpr uint32_t aw_mask = IN_MOVE|IN_DONT_FOLLOW|IN_ONLYDIR|IN_MASK_ADD;
 
-    int fd;
+    int fd, efd;
     std::thread thrd;
     wd_map_t wd_map;
-    // vector of events?
     bool shutdown{false};
 
-    void ev_loop() {
-      while(! shutdown) {
-	std::this_thread::sleep_for(200ms); // wait_for
+    class AlignedBuf
+    {
+      char* m;
+    public:
+      AlignedBuf() {
+	m = static_cast<char*>(aligned_alloc(__alignof__(struct inotify_event), rd_size));
+	if (! m) [[unlikely]] {
+	  std::cerr << fmt::format("{} buffer allocation failure", __func__) << std::endl;
+	  abort();
+	}
       }
-    }
+      ~AlignedBuf() {
+	std::free(m);
+      }
+      char* get() {
+	return m;
+      }
+    }; /* AlignedBuf */
+    
+    void ev_loop() {
+      std::unique_ptr<AlignedBuf> up_buf = std::make_unique<AlignedBuf>();
+      struct inotify_event* event;
+      char* buf = up_buf.get()->get();
+      ssize_t len;
+      int n;
+
+      nfds_t nfds{2};
+      struct pollfd fds[2] = {{fd, POLLIN}, {efd, POLLIN}};
+
+      while(! shutdown) {
+	n = poll(fds, nfds, -1); /* for up to 10 fds, poll is fast as epoll */
+	if (shutdown) {
+	  return;
+	}
+	if (n == -1) {
+	  if (errno = EINTR) {
+	    continue;
+	  }
+	  // XXX
+	}
+	if (n > 0) {
+	  len = read(fd, buf, rd_size);
+	  if (len == -1) {
+	    continue; // hopefully, was EAGAIN
+	  }
+	  for (char* ptr = buf; ptr < buf + len;
+	       ptr += sizeof(struct inotify_event) + event->len) {
+	    event = reinterpret_cast<struct inotify_event*>(ptr);
+	    if (event->mask & IN_Q_OVERFLOW) [[unlikely]] {
+	      /* cache blown, invalidate */
+	      /* TODO: implement */
+	    } else {
+	      if ((event->mask & IN_CREATE) ||
+		  (event->mask & IN_MOVED_TO)) {
+		/* new object in dir */
+	      } else if ((event->mask & IN_DELETE) ||
+			 (event->mask & IN_MOVED_FROM)) {
+		/* object removed from dir */
+	      
+	      }
+	    } /* !overflow */
+	  } /* events */
+	} /* n > 0 */
+      }
+    } /* ev_loop */
 
     Inotify(std::string& bucket_root)
       : Notify(bucket_root)
       {
-	fd = inotify_init1(0);
+	fd = inotify_init1(IN_NONBLOCK);
 	if (fd == -1) {
 	  std::cerr << fmt::format("{} inotify_init1 failed with {}", __func__, fd) << std::endl;
 	  exit(1);
 	}
+	efd = eventfd(0, EFD_NONBLOCK);
       }
+
+    void signal_shutdown() {
+      uint64_t msg{sig_shutdown};
+      (void) write(efd, &msg, sizeof(uint64_t));
+    }
 
     friend class Notify;
   public:
@@ -98,6 +170,8 @@ namespace file::listing {
     }
 
     virtual ~Inotify() {
+      shutdown = true;
+      signal_shutdown();
       thrd.join();
     }
   };
